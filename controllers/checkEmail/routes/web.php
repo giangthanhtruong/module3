87530a4ab54d1adef8c397abce3fceddd746d285
giangthanhtruong <giangthanhtruong88@gmail.com>
2021-11-01 16:18:55 +0700

    kk

--- controllers/checkEmail/routes/web.php
+++ controllers/checkEmail/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::post('/check-email', [App\Http\Controllers\UserController::class, 'validationEmail'])->name('checkEmail');
